a743da62bcf6cf0afbeeadceb094cb5ca6d9208b
meghan <lindholm.megh@gmail.com>
2018-05-12 23:41:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 console.log(name)
 var name = "Joe"
-
+if (name === "Joe"){console.log(" The name is correct")
 var height = "74"
-
 if (height ==="74"){console.log(" The height is correct")
 
 // Don't worry about this
